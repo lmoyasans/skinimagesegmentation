52953c675765504b772ce9d1f68f69a3c9fef8f3
rauldelafuente <raul.delafuentec@gmail.com>
2021-11-01 18:52:04 +0100

    improvement of the score and commented code

--- LS3_100383016_multithreading.py
+++ LS3_100383016_multithreading.py
@@ -1,42 +1,60 @@
 import os
 import numpy as np
-from skimage import io, filters, color
+from skimage import io, filters, color, morphology
 from sklearn.metrics import jaccard_score
-import threading
-from threading import Semaphore
-import time
+import matplotlib.pyplot as plt
 import multiprocessing
 
-sem = Semaphore(1)
 
 def preprocess(image):
-    image_preprocessed = filters.difference_of_gaussians(image, low_sigma = 120)
+    # First pre-processing technqiue: difference_of_gaussians
+    image_preprocessed = filters.difference_of_gaussians(image, low_sigma = 140)
+    # Second pre-processing technqiue: median filter
+    image_preprocessed = filters.median(image_preprocessed)
+    '''plt.figure()
+    plt.subplot(1,2,1), plt.imshow(image)
+    plt.subplot(1,2,2), plt.imshow(image_preprocessed)
+    plt.show()'''
     return image_preprocessed
 
 def segment(image):
+    # Perform OTSU segmentation:
+    # First we calculate the threshold
     otsu_th = filters.threshold_otsu(image)
+    # After that, we get the values lower than that threshold
     image_segmented = (image < otsu_th).astype('int')
     return image_segmented
 
 def postprocess(image):
-    predicted_mask = image
+    # First postprocess technqiue in order to isolate object from the rest: opening
+    predicted_mask = morphology.binary_opening(image)
+    # Second postprocess technqiue in order to fill the holes in the objects: dilation
+    predicted_mask = morphology.binary_dilation(predicted_mask)
+    '''plt.figure()
+    plt.subplot(1,2,1), plt.imshow(image)
+    plt.subplot(1,2,2), plt.imshow(predicted_mask)
+    plt.show()'''
     return predicted_mask
 
 
 def skin_lesion_segmentation(img_root):
+    # Read the image
     image = io.imread(img_root)
+    # Cinvert the incoming image into a gray image
     image_gray = color.rgb2gray(image)
+    # Preprocess the image
     image_gray_preprocessed = preprocess(image_gray)
+    # Segmenetation process
     image_segmented = segment(image_gray_preprocessed)
+    # Postprocessing of the image
     predicted_mask = postprocess(image_segmented)
     return predicted_mask
 
 def thread(img_roots,gt_mask_roots):
+    # Perform the segmentation with all its preprocessing and postprocessing
     predicted_mask = skin_lesion_segmentation(img_roots)
     gt_mask = io.imread(gt_mask_roots)/255 
-    #sem.acquire()   
-    #score.append(jaccard_score(np.ndarray.flatten(gt_mask),np.ndarray.flatten(predicted_mask)))
-    #sem.release()
+    # Get the jaccard score compraing the result with the mask of the set
     return jaccard_score(np.ndarray.flatten(gt_mask),np.ndarray.flatten(predicted_mask))
 
 def evaluate_masks(img_roots, gt_masks_roots):
@@ -52,18 +70,11 @@ def evaluate_masks(img_roots, gt_masks_roots):
         the list
     """
     score = []
-    NTHREADS = np.size(img_roots)
-
+    # We create a pool of mupltiprocess in order to perfrom a faster training
     pool_obj = multiprocessing.Pool()
+    # Get the score of each image after training
     score = pool_obj.starmap(thread, zip(img_roots,gt_masks_roots))
-    #t = threading.Thread(target=thread, args=[img_roots, gt_masks_roots])
-    #t.start()
-    #for i in np.arange(np.size(img_roots)):
-        #t = threading.Thread(target=thread, args=[img_roots[i], gt_masks_roots[i]])
-        #t.start()
-        #print('I%d' %i)
-        #score.append(t.join)
-
+    # Calculate the mean of the image scores
     mean_score = np.mean(score)
     print('Average Jaccard Index: '+str(mean_score))
     return mean_score
@@ -92,7 +103,4 @@ print("Number of image masks", len(train_masks_files))
 #     Segmentation and evaluation
 #
 # -----------------------------------------------------------------------------
-start = time.time()
 mean_score = evaluate_masks(train_imgs_files, train_masks_files)
-done = time.time()
-print(done - start)
